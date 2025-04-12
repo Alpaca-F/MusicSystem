0a8eef288d0884d2589d237e117e010f4d143100
Alpaca-F <439782826@qq.com>
2025-04-12 09:57:13 +0800

    first commit

---
+++ MusicSystemUserVue/src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Description: 
+ * @Author: 
+ * @Date: 2024-03-16 01:26:34
+-->
+<template>
+    <div class="the-footer">
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'the-footer'
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/css/the-footer.scss';
+</style>
